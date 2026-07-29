851701be0b2be911b74982934d2a979e3feb150b
Stone Tao <stonezt2019@gmail.com>
2026-07-28 23:09:03 -0700

    init rich logger

---
+++ mani_skill/utils/logging_utils.py
@@ -0,0 +1,11 @@
+import logging
+
+from rich.logging import RichHandler
+
+logger = logging.getLogger("mani_skill ")
+logger.setLevel(logging.INFO)
+logger.propagate = False
+if not logger.hasHandlers():
+    ch = RichHandler(rich_tracebacks=True)
+    ch.setFormatter(logging.Formatter("%(name)s - %(message)s"))
+    logger.addHandler(ch)
