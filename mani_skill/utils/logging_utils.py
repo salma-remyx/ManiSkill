1c0ba984a5d6b0cbabcf1167bc8b53b89470f7fe
Stone Tao <stonezt2019@gmail.com>
2026-07-28 23:09:56 -0700

    Update logging_utils.py

--- mani_skill/utils/logging_utils.py
+++ mani_skill/utils/logging_utils.py
@@ -2,10 +2,10 @@ import logging
 
 from rich.logging import RichHandler
 
-logger = logging.getLogger("mani_skill ")
+logger = logging.getLogger("mani_skill")
 logger.setLevel(logging.INFO)
 logger.propagate = False
 if not logger.hasHandlers():
     ch = RichHandler(rich_tracebacks=True)
-    ch.setFormatter(logging.Formatter("%(name)s - %(message)s"))
+    ch.setFormatter(logging.Formatter("%(name)s: %(message)s"))
     logger.addHandler(ch)
